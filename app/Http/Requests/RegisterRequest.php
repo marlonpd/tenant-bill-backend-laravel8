0709851e6b2f3b44b09f6e9fde6a10f8df082ba6
marlonpd <send8heremd@gmail.com>
2021-01-25 09:32:16 +0800

    Add log and reg request

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:200',
+            'email' => 'email|unique:users,email', 
+            'password' => 'min:6|required_with:password2|same:password2',
+            'password2' => 'min:6'
+        ];
+    }
+}
