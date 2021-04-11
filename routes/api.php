538fb235d57f865447b3bad4a190083011dd7aff
marlonpd <send8heremd@gmail.com>
2021-04-11 21:23:17 +0800

    Implement laravel 8 routing

--- routes/api.php
+++ routes/api.php
@@ -32,21 +32,20 @@ Route::middleware('api')->namespace('App\Http\Controllers')->group(function () {
         Route::get('/tenants', 'TenantController@index')->name('fetchAllTenants');
         Route::get('/tenants/{pageIndex}/get', 'TenantController@getLimitedList')->name('getLimitedList');
         Route::post('/tenant/store', 'TenantController@store')->name('storeTenant');
-        Route::post('/tenant/delete', 'TenantController@destroy')->name('deleteTenant');
-        Route::post('/tenant/update', 'TenantController@update')->name('updateTenant');
+        Route::delete('/tenant/delete', 'TenantController@destroy')->name('deleteTenant');
+        Route::put('/tenant/update', 'TenantController@update')->name('updateTenant');
         Route::get('/tenant/{id}', 'TenantController@show')->name('showTenant');
     
         Route::get('/meter-readings/{tenantId}', 'MeterReadingController@index')->name('fetchAllMeterReadings');
-        Route::post('/meter-reading/update', 'MeterReadingController@update')->name('updateMeterReading');
+        Route::put('/meter-reading/update', 'MeterReadingController@update')->name('updateMeterReading');
         Route::post('/meter-reading/store', 'MeterReadingController@store')->name('storeMeterReading');
-        Route::post('/meter-reading/delete', 'MeterReadingController@destroy')->name('deleteMeterReading');
+        Route::delete('/meter-reading/delete', 'MeterReadingController@destroy')->name('deleteMeterReading');
         Route::get('/meter-readings/$tenantId/{pageIndex}/get', 'MeterReadingController@getLimitedList')->name('getLimitedList');
-        //Route::get('/meter-reading/{id}', 'MeterReadingController@show')->name('showMeterReading');
     
         Route::get('/power-rates', 'PowerRateController@index')->name('fetchAllPowerRates');
         Route::post('/power-rate/store', 'PowerRateController@store')->name('storePowerRate');
-        Route::post('/power-rate/delete', 'PowerRateController@destroy')->name('deletePowerRate');
-        Route::post('/power-rate/update', 'PowerRateController@update')->name('updatePowerRate');
+        Route::delete('/power-rate/delete', 'PowerRateController@destroy')->name('deletePowerRate');
+        Route::put('/power-rate/update', 'PowerRateController@update')->name('updatePowerRate');
         Route::get('/power-rate/{id}', 'PowerRateController@show')->name('showPowerRate');
         Route::get('/power-rates/{pageIndex}/get', 'PowerRateController@getLimitedList')->name('getLimitedList');
     });
